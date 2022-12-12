731b7b7b9eec9ba452b640e6ff8324f471e5c698
Tarin <tarin@Zipcoders-MacBook-Pro-5.local>
2022-12-12 13:59:59 -0500

    created and passed personConstructorTest

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -3,11 +3,29 @@ package io.zipcoder.interfaces;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Date;
+
 public class TestPerson {
     final long id = 0;
     String name = "";
     @Test
     public void personConstructorTest() {
+        // Given (cat data)
+        String givenName = "Rin";
+        long givenId = 123;
+
+        // When (a cat is constructed)
+        Person person = new Person(givenId, givenName);
+
+        // When (we retrieve data from the cat)
+        String retrievedName = person.getName();
+        long retrievedId = person.getId();
+
+        // Then (we expect the given data, to match the retrieved data)
+        Assert.assertEquals(givenName, retrievedName);
+        Assert.assertEquals(givenId, retrievedId);
 
     }
+
+
 }
